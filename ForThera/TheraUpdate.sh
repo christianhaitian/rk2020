ee0d125f1bc12e88266090de78208536f0a3ba11
christianhaitian <mckman@comcast.net>
2020-10-17 15:36:28 -0400

    Atari800 Fix

--- ForThera/TheraUpdate.sh
+++ ForThera/TheraUpdate.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
-msgbox "No updates available at this time.  Check back later."
+clear
+
+UPDATE_DATE="10172020"
+LOG_FILE="/home/odroid/update$UPDATE_DATE.log"
+UPDATE_DONE="/home/odroid/.config/testupdate$UPDATE_DATE"
+
+if [ -f "$UPDATE_DONE" ]; then
+  msgbox "No more updates available.  Check back later."
+  rm -- "$0"
+  exit 187
+fi
+
+if [ -f "$LOG_FILE" ]; then
+  sudo rm "$LOG_FILE"
+fi
+
+printf "\nInstalling Atari800 fix...\n" | tee -a "$LOG_FILE"
+sudo wget https://github.com/christianhaitian/rk2020/raw/master/ForThera/Update3.1/Atari800sep.tar -a "$LOG_FILE"
+sudo mv -v /etc/emulationstation/es_systems.cfg /etc/emulationstation/es_systems.cfg.update$UPDATE_DATE.bak | tee -a "$LOG_FILE"
+sudo tar -xvf Atari800sep.zip / | tee -a "$LOG_FILE"
+sudo chown -v odroid:odroid -R /home/odroid/.config/retroarch/config/Atari800/ | tee -a "$LOG_FILE"
+sudo chown -v odroid:odroid -R /home/odroid/.config/retroarch/config/remaps/Atari800/ | tee -a "$LOG_FILE"
+sudo chown -v odroid:odroid -R /home/odroid/.atari800.cfg | tee -a "$LOG_FILE"
+sudo chown -v odroid:odroid /etc/emulationstation/es_systems.cfg | tee -a "$LOG_FILE"
+msgbox "Updates have been completed.  Hit A to go back to Emulationstation."
+touch "$UPDATE_DONE"
+rm -v -- "$0" | tee -a "$LOG_FILE"
+sudo systemctl restart emulationstation
 exit 187
---
+++ ForThera/atari800fix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo wget https://github.com/christianhaitian/rk2020/raw/master/ForThera/Update3.1/Atari800sep.tar
+sudo mv -v /etc/emulationstation/es_systems.cfg /etc/emulationstation/es_systems.cfg.temp101720.bak
+sudo tar -xf Atari800sep.zip /
+sudo chown odroid:odroid -R /home/odroid/.config/retroarch/config/Atari800/
+sudo chown odroid:odroid -R /home/odroid/.config/retroarch/config/remaps/Atari800/
+sudo chown odroid:odroid -R /home/odroid/.atari800.cfg
+sudo chown odroid:odroid /etc/emulationstation/es_systems.cfg
+sudo systemctl restart emulationstation
+rm -- "$0"
