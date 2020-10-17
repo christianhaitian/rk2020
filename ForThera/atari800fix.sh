d7b6840c6fd14c2f74678af0569ddc71610a3577
christianhaitian <mckman@comcast.net>
2020-10-17 15:46:20 -0400

    Update to resolve naming issue

--- ForThera/atari800fix.sh
+++ ForThera/atari800fix.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo wget https://github.com/christianhaitian/rk2020/raw/master/ForThera/Update3.1/Atari800sep.tar
 sudo mv -v /etc/emulationstation/es_systems.cfg /etc/emulationstation/es_systems.cfg.temp101720.bak
-sudo tar -xf Atari800sep.zip /
+sudo tar -xf Atari800sep.tar /
+sudo rm -v Atari800sep.tar
 sudo chown odroid:odroid -R /home/odroid/.config/retroarch/config/Atari800/
 sudo chown odroid:odroid -R /home/odroid/.config/retroarch/config/remaps/Atari800/
 sudo chown odroid:odroid -R /home/odroid/.atari800.cfg
